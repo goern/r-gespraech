3cffdca996e11cac3e98e9370be1713f29e310c1
Christoph Görn <goern@b4mad.net>
2022-05-17 18:17:10 +0200

    :arrow_up: make setting up the webhooks configurable: ENABLE_WEBHOOKS={true|false}
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- main.go
+++ main.go
@@ -88,9 +88,11 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Callback")
 		os.Exit(1)
 	}
-	if err = (&webhookv1alpha1.Callback{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Callback")
-		os.Exit(1)
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&webhookv1alpha1.Callback{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Callback")
+			os.Exit(1)
+		}
 	}
 	//+kubebuilder:scaffold:builder
 
